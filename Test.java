026639cd76c274f8b385da031d469b3d52a84cd2
Mounika <mounika@bpatech.com>
2015-11-18 14:07:02 +0530

    Added 2 d's in sop

--- Test.java
+++ Test.java
@@ -12,7 +12,7 @@ public class FinallyEx
          }  
 finally
        {
-        System.out.println("Change");
+        System.out.println("Changedd");
       /*  System.out.println("Welcome");
         System.out.println("finally block is executed");*/
        }  
